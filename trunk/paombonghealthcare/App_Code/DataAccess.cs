3d61375b9ad7b2ad588f8edc233a7f65c9858b01
superdevLester <superdevLester@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-10-06 04:11:04 +0000

    initial project source code

---
+++ trunk/paombonghealthcare/App_Code/DataAccess.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+/* Log Positive
+ *      1. Add Patient Finished - Lakhi 10/5/2010
+ *      2. Update Patient Finished - Lakhi 10/5/2010
+ *      3. Get Values Finished - Lakhi 10/5/2010
+ *      4. 
+ */
+
+/* Log Negative
+ *      1. BarangayId Not yet Fixed in Update and Get Values - Lakhi 10/5/2010
+ *      2. 
+ *      3. 
+ *      4. 
+ */
+
+public class DataAccess
+{
+    private string dataConnection = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Lakhi\Desktop\db_paombong\paombongdb.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+	
+    public DataAccess()
+	{
+		
+	}
+
+    /*Add Patient Finished - Lakhi 10/5/2010*/
+
+    public void AddPatient(
+        string Patient_Id, string PatientFirstName, string PatientMiddleName, string PatientLastName,
+        string PatientContactNumber, string PatientEmailAddress, string PatientSuffix, string PatientBirthdate,string PatientBirthplace, string PatientAddress,
+        string PatientFaxNumber, string PatientDoctor, string PatientNationality, string PatientCity,
+        string PatientSex, string PatientMaritalStatus, string PatientSpouseName,string PatientCompany, string DateRegistered, string PatientBarangay)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        try
+        {
+            connPatient.Open();
+            SqlCommand cmdTxt = new SqlCommand("AddPatient", connPatient);
+            cmdTxt.CommandType = CommandType.StoredProcedure;
+            cmdTxt.Parameters.Add("@Patient_Id", SqlDbType.Int).Value = Patient_Id;
+            cmdTxt.Parameters.Add("@PatientFirstName", SqlDbType.Char).Value = PatientFirstName;
+            cmdTxt.Parameters.Add("@PatientMiddleName", SqlDbType.Char).Value = PatientMiddleName;
+            cmdTxt.Parameters.Add("@PatientLastName", SqlDbType.Char).Value = PatientLastName;
+            cmdTxt.Parameters.Add("@PatientContactNumber", SqlDbType.Char).Value = PatientContactNumber;
+            cmdTxt.Parameters.Add("@PatientEmailAddress", SqlDbType.Char).Value = PatientEmailAddress;
+            cmdTxt.Parameters.Add("@PatientNationality", SqlDbType.Char).Value = PatientNationality;
+            cmdTxt.Parameters.Add("@PatientCity", SqlDbType.Char).Value = PatientCity;
+            cmdTxt.Parameters.Add("@PatientBirthdate", SqlDbType.Char).Value = PatientBirthdate;
+            cmdTxt.Parameters.Add("@PatientBirthPlace", SqlDbType.Char).Value = PatientBirthplace;
+            cmdTxt.Parameters.Add("@PatientAddress", SqlDbType.Char).Value = PatientAddress;
+            cmdTxt.Parameters.Add("@PatientFaxNumber", SqlDbType.Char).Value = PatientFaxNumber;
+            cmdTxt.Parameters.Add("@PatientDoctor", SqlDbType.Char).Value = PatientDoctor;
+            cmdTxt.Parameters.Add("@PatientSex", SqlDbType.Char).Value = PatientSex;
+            cmdTxt.Parameters.Add("@PatientCompany", SqlDbType.Char).Value = PatientCompany;
+            cmdTxt.Parameters.Add("@PatientMaritalStatus", SqlDbType.Char).Value = PatientMaritalStatus;
+            cmdTxt.Parameters.Add("@PatientSpouseName", SqlDbType.Char).Value = PatientSpouseName;
+            cmdTxt.Parameters.Add("@DateRegistered", SqlDbType.Char).Value = DateRegistered;
+            cmdTxt.Parameters.Add("@PatientSuffix", SqlDbType.Char).Value = PatientSuffix;
+            cmdTxt.Parameters.Add("@PatientBarangay", SqlDbType.Char).Value = PatientBarangay;
+
+            int checker = cmdTxt.ExecuteNonQuery();
+            if (checker > 0)
+                MessageBox.Show("Patient Saved!");
+            else
+                MessageBox.Show("Patient Not Saved! Please Try Again");
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error : "+ ex.Message);
+        }
+        finally
+        {
+            connPatient.Close();
+        }
+    }
+
+    public DataTable GetValues(string Patient_Id)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        SqlDataReader dtrPatient;
+        DataTable patientData = new DataTable();
+        patientData.Columns.Add("PatientID");
+        patientData.Columns.Add("PatientName");
+        patientData.Columns.Add("PatientSex");
+        patientData.Columns.Add("PatientCompany");
+        patientData.Columns.Add("PatientCity");
+        patientData.Columns.Add("PatientBirthdate");
+        patientData.Columns.Add("PatientBirthplace");
+        patientData.Columns.Add("PatientCivilStatus");
+        patientData.Columns.Add("SpouseName");
+        patientData.Columns.Add("PatientNationality");
+        patientData.Columns.Add("PatientAddress"); 
+        patientData.Columns.Add("PatientContactNumber");
+        patientData.Columns.Add("PatientEmailAddress");
+        patientData.Columns.Add("PatientFaxNumber");
+        patientData.Columns.Add("PatientDoctor");  
+        patientData.Columns.Add("DateRegistered");
+        //patientData.Columns.Add("PatientBarangay");
+
+        connPatient.Open();
+        try
+        {
+            SqlCommand cmdTxt = new SqlCommand("GetPatientData", connPatient);
+            cmdTxt.CommandType = CommandType.StoredProcedure;
+            cmdTxt.Parameters.Add("@Patient_Id", SqlDbType.Char).Value = Patient_Id;
+            dtrPatient = cmdTxt.ExecuteReader();
+            dtrPatient.Read();
+
+            patientData.Rows.Add(dtrPatient["PatientID"].ToString(),
+                dtrPatient["PtLname"].ToString().Trim() + "," + dtrPatient["PtFname"].ToString().Trim() + ","
+                + dtrPatient["PtMname"].ToString().Trim() + "," + dtrPatient["PtSuffix"].ToString().Trim(),
+                dtrPatient["PtGender"].ToString().Trim(),
+                dtrPatient["PtCompany"].ToString().Trim(),
+                dtrPatient["PtBusinessCty"].ToString().Trim(),
+                dtrPatient["PtBdate"].ToString().Trim(),
+                dtrPatient["PtBplace"].ToString().Trim(),
+                dtrPatient["CivilStatus"].ToString().Trim(),
+                dtrPatient["SpouseName"].ToString().Trim(),
+                dtrPatient["Nationality"].ToString().Trim(),
+                dtrPatient["PtAddress"].ToString().Trim(),
+                dtrPatient["PtContact"].ToString().Trim(),
+                dtrPatient["PtEmail"].ToString().Trim(),
+                dtrPatient["PtFaxNumber"].ToString().Trim(),
+                dtrPatient["PtDoctor"].ToString().Trim(),
+                dtrPatient["DateRegistered"].ToString().Trim());
+                //dtrPatient["BarangayID"].ToString().Trim());
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error : " + ex.Message);
+        }
+        finally
+        {
+            connPatient.Close();
+        }
+        return patientData;
+    }
+
+    /*Update Record Finished - Lakhi 10/5/2010*/
+
+    public void UpdateRecord(string Patient_Id, string PatientFirstName, string PatientMiddleName, string PatientLastName,
+        string PatientContactNumber, string PatientEmailAddress, string PatientSuffix, string PatientBirthdate, string PatientBirthplace, string PatientAddress,
+        string PatientFaxNumber, string PatientDoctor, string PatientNationality, string PatientCity,
+        string PatientSex, string PatientMaritalStatus, string PatientSpouseName, string PatientCompany, string DateRegistered, string PatientBarangay)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        connPatient.Open();
+        try
+        {
+            SqlCommand cmdTxt = new SqlCommand("UpdatePatient", connPatient);
+            cmdTxt.CommandType = CommandType.StoredProcedure;
+            cmdTxt.Parameters.Add("@Patient_Id", SqlDbType.Int).Value = Patient_Id;
+            cmdTxt.Parameters.Add("@PatientFirstName", SqlDbType.Char).Value = PatientFirstName;
+            cmdTxt.Parameters.Add("@PatientMiddleName", SqlDbType.Char).Value = PatientMiddleName;
+            cmdTxt.Parameters.Add("@PatientLastName", SqlDbType.Char).Value = PatientLastName;
+            cmdTxt.Parameters.Add("@PatientContactNumber", SqlDbType.Char).Value = PatientContactNumber;
+            cmdTxt.Parameters.Add("@PatientEmailAddress", SqlDbType.Char).Value = PatientEmailAddress;
+            cmdTxt.Parameters.Add("@PatientNationality", SqlDbType.Char).Value = PatientNationality;
+            cmdTxt.Parameters.Add("@PatientCity", SqlDbType.Char).Value = PatientCity;
+            cmdTxt.Parameters.Add("@PatientBirthdate", SqlDbType.Char).Value = PatientBirthdate;
+            cmdTxt.Parameters.Add("@PatientBirthPlace", SqlDbType.Char).Value = PatientBirthplace;
+            cmdTxt.Parameters.Add("@PatientAddress", SqlDbType.Char).Value = PatientAddress;
+            cmdTxt.Parameters.Add("@PatientFaxNumber", SqlDbType.Char).Value = PatientFaxNumber;
+            cmdTxt.Parameters.Add("@PatientDoctor", SqlDbType.Char).Value = PatientDoctor;
+            cmdTxt.Parameters.Add("@PatientSex", SqlDbType.Char).Value = PatientSex;
+            cmdTxt.Parameters.Add("@PatientCompany", SqlDbType.Char).Value = PatientCompany;
+            cmdTxt.Parameters.Add("@PatientMaritalStatus", SqlDbType.Char).Value = PatientMaritalStatus;
+            cmdTxt.Parameters.Add("@PatientSpouseName", SqlDbType.Char).Value = PatientSpouseName;
+            cmdTxt.Parameters.Add("@DateRegistered", SqlDbType.Char).Value = DateRegistered;
+            cmdTxt.Parameters.Add("@PatientSuffix", SqlDbType.Char).Value = PatientSuffix;
+            //cmdTxt.Parameters.Add("@PatientBarangay", SqlDbType.Char).Value = PatientBarangay;
+            int check = cmdTxt.ExecuteNonQuery();
+            if (check > 0)
+                MessageBox.Show("Updated Patient Information Successfully!");
+            else
+                MessageBox.Show("Unsuccesfull!! Updating Patient Information Please Try Again!");
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error: " + ex.Message);
+        }
+        finally
+        {
+            connPatient.Close();
+        }
+    }
+
+    public DataTable ViewMedicalRecord(string Patient_Id)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        SqlDataReader dtrPatient;
+        DataTable dataPatient = new DataTable();
+        dataPatient.Columns.Add("Patient_Id");
+        dataPatient.Columns.Add("MedicalRecord");
+        dataPatient.Columns.Add("DateOfCheckUp");
+        connPatient.Open();
+        try
+        {
+            SqlCommand cmdTxt = new SqlCommand("ViewHealthRecord", connPatient);
+            cmdTxt.CommandType = CommandType.StoredProcedure;
+            cmdTxt.Parameters.Add("@Patient_Id", SqlDbType.Char).Value = Patient_Id;
+            dtrPatient = cmdTxt.ExecuteReader();
+            while (dtrPatient.Read())
+            {
+                dataPatient.Rows.Add(dtrPatient["Patient_Id"].ToString().Trim(),dtrPatient["MedicalRecord"].ToString().Trim(),
+                    dtrPatient["DateOfCheckUp"].ToString().Trim());
+            }
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error: " + ex.Message);
+        }
+        finally
+        {
+            connPatient.Close();
+        }
+        return dataPatient;
+    }
+
+    public void AddRecord(string Patient_Id, string MedicalRecord)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        try
+        {
+            connPatient.Open();
+            SqlCommand cmdTxt = new SqlCommand("InsertRecord", connPatient);
+            cmdTxt.CommandType = CommandType.StoredProcedure;
+            cmdTxt.Parameters.Add("@Patient_Id", SqlDbType.Char).Value = Patient_Id;
+            cmdTxt.Parameters.Add("@MedicalRecord", SqlDbType.Char).Value = MedicalRecord;
+            cmdTxt.Parameters.Add("@DateOfCheckUp", SqlDbType.Char).Value = DateTime.Now.ToString("d");
+            int checker = cmdTxt.ExecuteNonQuery();
+            if (checker > 0)
+                MessageBox.Show("Successfully Added Record");
+            else
+                MessageBox.Show("Please Try Again!!");
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error : " + ex.Message);
+        }
+        finally
+        {
+            connPatient.Close();
+        }
+    }
+    public int Checker(string Patient_Id)
+    {
+        SqlConnection connPatient = new SqlConnection(dataConnection);
+        connPatient.Open();
+        SqlCommand cmdTxt = new SqlCommand("Select Count(*) From Patient Where Patient_Id = @aa",connPatient);
+        cmdTxt.Parameters.Add("@aa", SqlDbType.Char).Value = Patient_Id;
+        int count = (int)cmdTxt.ExecuteScalar();
+        connPatient.Close();
+        return count;
+
+    }
+}
---
+++ trunk/paombonghealthcare/Medical Record/MedicalInventory.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteTemplate.master" AutoEventWireup="true" CodeFile="MedicalInventory.aspx.cs" Inherits="Medical_Record_MedicalInventory" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
