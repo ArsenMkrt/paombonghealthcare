da9e92363a9ba37be1c805e96a3f074652853cae
magno.gerald <magno.gerald@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-11-26 05:26:55 +0000

    created login module, user authentication, page restrictions

---
+++ HISystem/Public/Register.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class Public_Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        RegisterUser.ContinueDestinationPageUrl = Request.QueryString["ReturnUrl"];
+    }
+
+    protected void RegisterUser_CreatedUser(object sender, EventArgs e)
+    {
+        FormsAuthentication.SetAuthCookie(RegisterUser.UserName, false /* createPersistentCookie */);
+
+        string continueUrl = RegisterUser.ContinueDestinationPageUrl;
+        if (String.IsNullOrEmpty(continueUrl))
+        {
+            continueUrl = "~/";
+        }
+        Response.Redirect(continueUrl);
+    }
+}
