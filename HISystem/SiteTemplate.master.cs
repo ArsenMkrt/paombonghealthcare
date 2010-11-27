ccd3d83cc270150aa131e2915b3d6912d25c685a
magno.gerald <magno.gerald@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-11-27 17:30:37 +0000

    checks role of logged in user to manage visibility of hyperlinktoAdminPage

--- HISystem/SiteTemplate.master.cs
+++ HISystem/SiteTemplate.master.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class SiteTemplate : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            if (!Roles.IsUserInRole(HttpContext.Current.User.Identity.Name, "Doctor"))
+            {
+
+                //make hyperlink invisible if not doctor role
+                HyperLinktoAdminPage.Visible = false;
+                return;
+            }
+        }
+        catch (HttpException exc)
+        {
+            //display error msg
+            return;
+        }
 
     }
 }
