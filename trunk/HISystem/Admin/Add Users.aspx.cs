40c04ea8b66190fdcd523e8629d3e640a3291b7b
magno.gerald <magno.gerald@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-11-27 16:35:28 +0000

    admin module to add users and assign role

---
+++ trunk/HISystem/Admin/Add Users.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Add_Users : System.Web.UI.Page
+{
+   
+}
