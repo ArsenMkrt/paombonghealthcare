abfc6a96e0cf17b9701c60c3cd0f02f8385e84da
magno.gerald <magno.gerald@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-11-29 18:57:25 +0000

    Bulk insert module added, automatic total in jscript added, duplicated Childcare table to create auto-increment in cChildcare1 table created,
    note:
    in creating pk fields in tables: set is identity to true for auto increment

---
+++ HISystem/Reports/Bulk/Childcare.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Reports_Bulk_Childcare : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            BindData();
+        }
+    }
+
+    private void BindData()
+    {
+        // string constr = "Data Source=GERALD-PC\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Magno\\Desktop\\HISystem\\App_Data\\paombongdb.mdf;Initial Catalog=phis;Integrated Security=True";
+        string constr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Jay\\Desktop\\HISystem\\App_Data\\paombongdb.mdf;Integrated Security=True;User Instance=True";
+        string query = "SELECT BarangayID, BarangayName FROM Barangays";
+
+        SqlDataAdapter da = new SqlDataAdapter(query, constr);
+        DataTable table = new DataTable();
+
+        da.Fill(table);
+
+        ListView1.DataSource = table;
+        ListView1.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (dropIndicator.SelectedValue != null && dropIndicator.SelectedItem!=null)
+        {
+            
+            //string constr = "Data Source=GERALD-PC\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Magno\\Desktop\\HISystem\\App_Data\\paombongdb.mdf;Initial Catalog=phis;Integrated Security=True";
+            string constr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Jay\\Desktop\\HISystem\\App_Data\\paombongdb.mdf;Integrated Security=True;User Instance=True";
+            foreach (ListViewDataItem lv in this.ListView1.Items)
+            {
+
+
+                //ListViewItem item = ListView1.Items;
+                Label LBrgyID = (Label)lv.FindControl("lblBrgyID");
+
+                Label LBrgy = (Label)lv.FindControl("lblBrgy");
+                TextBox tMale = (TextBox)lv.FindControl("txtMale");
+                TextBox tFemale = (TextBox)lv.FindControl("txtFemale");
+                TextBox tTotal1 = (TextBox)lv.FindControl("txtTotal");
+                
+
+
+                // insert records into database
+                using (SqlConnection conn = new SqlConnection(constr))
+                {
+                    //eto yung ayaw mainsert
+                    string Sql = "insert into cChildCare1 (ChildData, BarangayID, [1st_Male], [1st_Female], [1st_Total]) values (@progIndicator, @BgyID, @Male, @Female, @Total1)";
+
+                    //eto yung naiinsert pag wala na ung int values sice allow null naman ung tinangall ko
+                    //string Sql = "insert into cChildCare (ChildData, BarangayID) values (@BName, @BgyID)";
+                    conn.Open();
+                    using (SqlCommand dCmd = new SqlCommand(Sql, conn))
+                    {
+                        if (dropIndicator.SelectedItem.ToString() != null)
+                        {
+                            dCmd.Parameters.AddWithValue("progIndicator", dropIndicator.SelectedItem.ToString());
+                        }
+                        else if (dropIndicator.SelectedItem.ToString() == null)
+                        {
+                            dCmd.Parameters.AddWithValue("progIndicator",LBrgy.Text);
+                        }
+                        
+                        dCmd.Parameters.AddWithValue("@BgyID", LBrgyID.Text);
+
+
+                        dCmd.Parameters.AddWithValue("@Male", Convert.ToInt32(tMale.Text.Trim()));
+                        dCmd.Parameters.AddWithValue("@Female", Convert.ToInt32(tFemale.Text.Trim()));
+                        dCmd.Parameters.AddWithValue("@Total1", Convert.ToInt32(tTotal1.Text.Trim()));
+
+
+
+
+                        dCmd.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                }
+                Label1.Text = "Records Inserted Successfully.";
+                
+            }
+            Response.Write("<script> window.alert('Records Inserted Successfully.')</script>");
+        }
+        else
+            Response.Write("<script> window.alert('Please supply program')</script>");    
+        
+
+    }
+}
