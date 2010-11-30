78db1f17d1dfe599d31a832baaa64171319840c1
superdevLester <superdevLester@b5103d38-e3f9-40c5-ab76-e6f3131df775>
2010-11-30 14:38:01 +0000

    Report not yet finished.. kinakapa ko palang heheheh!:))

---
+++ trunk/HISystem/DataSetMonthlyReport.cs
@@ -0,0 +1,22 @@
+﻿namespace Paumbong_Project {
+
+
+    partial class DataSetMonthlyReport
+    {
+        partial class PopulationDataTable
+        {
+        }
+    
+        partial class MaternalCareDataTable
+        {
+        }
+    
+        partial class BrgyListDataTable
+        {
+        }
+    
+        partial class MalariaDataTable
+        {
+        }
+    }
+}
